0793b8bcf7ed201fcb666456b35f311f7f4696a3
Adela Coroiu <coroiuadela@gmail.com>
2021-09-18 12:50:25 +0300

    Tema hello world, functioneaza corect

---
+++ Tema1_HelloWorld/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int a,int b){
+return a+b;}
+
+void sumaTest()
+int a = 5, b=3;
+int c = suma(a,b);
+... /// verificare pentru suma 8
+
+a= 3, b -1
+//verificare
+
+a= nr foarte mare, b = nr foarte foarte mare
+//verificare
+}
+g
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
